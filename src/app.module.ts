f249aaf334fa713f6254d1b059ee0afeb47650a0
armandopato <armauv@gmail.com>
2020-08-05 19:21:35 -0500

    Add typeOrm config and create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { PetitionsModule } from './petitions/petitions.module';
+import { ResolutionsModule } from './resolutions/resolutions.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    AuthModule,
+    PetitionsModule,
+    ResolutionsModule,
+    UserModule
+  ],
   controllers: [AppController]
 })
 export class AppModule {}
